e332c349a463ebdf053772db4eb3e5f99145a151
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-02-03 19:18:41 +0400

    Add files via upload

---
+++ homeWork1/ConsoleApp/src/Loader.java
@@ -0,0 +1,14 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Loader
+{
+    public static void main(String args[])
+    {
+        //Текущаяя дата.
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm dd.MM.yyyy");
+        Date date = new Date ();
+        System.out.println(dateFormat.format(date));
+    }
+}
