093c8239ce956bff922de3b58a7d1d9d74e03d78
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-05-05 15:55:36 +0400

    finally version

--- 05_ArraysAndCollections/05_PhoneBook/src/Solution.java
+++ 05_ArraysAndCollections/05_PhoneBook/src/Solution.java
@@ -1,6 +1,4 @@
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Solution {
 
@@ -34,9 +32,6 @@ public class Solution {
 
         for (; ; ) {
 
-            String number = "";
-            String name = "";
-
             System.out.print("Введи имя или номер телефона: ");
             String userInput = scanner.nextLine();
 
@@ -44,66 +39,79 @@ public class Solution {
                 return userInput;
             }
 
-            if (userInput.matches("[0-9]+") &&
-                    (userInput.length() >= 10 && userInput.length() <= 11)) {
-                number = userInput;
+            if (userInput.matches("[0-9]+") && (userInput.length() >= 10 && userInput.length() <= 11)) {
+                if (!phoneBook.containsKey(userInput)) {
+                    System.out.print("Введи имя: ");
+                    for (; ; ) {
+                        String input = scanner.nextLine();
+                        if (input.matches("[A-я\\s0-9]+")) {
+                            phoneBook.put(userInput, input);
+                            System.out.println("\nНовый пользователь!\nИмя: " + input + "\nНомер: " + userInput);
+                            System.out.println();
+                            break;
+                        } else {
+                            if (input.isBlank()) {
+                                System.out.println("Введи имя, пожалуйста!");
+                            }
+                        }
+                    }
+                    continue;
+                }
+
+                if (phoneBook.containsKey(userInput)) {
+                    System.out.println("\nТакой контакт уже есть:\n" +
+                            "Имя: " + phoneBook.get(userInput) + "\nНомер: " + userInput);
+                    System.out.println();
+                }
+
             } else {
+                if (phoneBook.containsValue(userInput)) {
+                    for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+                        System.out.println("\nТакой контакт уже есть:\n" +
+                                "Имя: " + entry.getValue() + "\nНомер: " + entry.getKey() + "\n");
+
+                    }
+                    System.out.println("Добавить еще одного человека с таким именем?\nДа или Нет");
+                    String repeatName = scanner.nextLine();
+                    if (repeatName.equalsIgnoreCase("нет")) {
+                        continue;
+                    }
+                }
+
                 if (userInput.isBlank()) {
                     continue;
-                } else {
-                    name = userInput;
                 }
-            }
 
-            if (!phoneBook.containsKey(userInput) && number.equals(userInput)) {
-                System.out.print("Введи имя: ");
-                for (; ; ) {
-                    String input = scanner.nextLine();
-                    if (input.matches("[A-я\\s0-9]+")) {
-                        phoneBook.put(userInput, input);
-                        System.out.println("\nНовый пользователь!\nИмя: " + input + "\nНомер: " + userInput);
-                        System.out.println();
-                        break;
-                    } else {
-                        if (input.isBlank()) {
-                            System.out.println("Введи имя, пожалуйста!");
+                if (!phoneBook.containsKey(userInput)) {
+                    System.out.print("Введи номер телефона: ");
+                    for (; ; ) {
+                        String input = scanner.nextLine();
+                        if (input.matches("[0-9]+") && (input.length() >= 10 && input.length() <= 11)) {
+                            phoneBook.put(input, userInput);
+                            System.out.println("\nНовый пользователь!\nИмя: " + userInput + "\nНомер: " + input);
+                            System.out.println();
+                            break;
+                        } else {
+                            System.out.println("Ошибка! \nНомер телефона должен состоять из 10 или 11 цифр. " +
+                                    "\nПопробуй еще раз ввести номер: ");
                         }
                     }
                 }
-                continue;
             }
-
-            if (!phoneBook.containsKey(userInput) && name.equals(userInput)) {
-                System.out.print("Введи номер телефона: ");
-                for (; ; ) {
-                    String input = scanner.nextLine();
-                    if (input.matches("[0-9]+") && (input.length() >= 10 && input.length() <= 11)) {
-                        phoneBook.put(input, userInput);
-                        System.out.println("\nНовый пользователь!\nИмя: " + userInput + "\nНомер: " + input);
-                        System.out.println();
-                        break;
-                    } else {
-                        System.out.println("Ошибка! \nНомер телефона должен состоять из 10 или 11 цифр. " +
-                                "\nПопробуй еще раз ввести номер: ");
-                    }
-                }
-            }
-
-            if (phoneBook.containsKey(userInput)) {
-                System.out.println("\nТакой контакт уже есть:\n" +
-                        "Имя: " + phoneBook.get(userInput) + "\nНомер: " + userInput);
-                System.out.println();
-            }
-
             return userInput;
         }
 
     }
 
     private static void printPhoneBook() {
-        System.out.println();
-        for (String key : ((Map<String, String>) Solution.phoneBook).keySet()) {
-            System.out.println("Номер телефона: " + key + ", Имя: " + ((Map<String, String>) Solution.phoneBook).get(key));
+        TreeSet<String> sortedTest = new TreeSet<>();
+
+        for (String s : phoneBook.keySet()) {
+            sortedTest.add("Имя: " + phoneBook.get(s) + ", Телефон: " + s);
+        }
+
+        for (String s : sortedTest) {
+            System.out.println(s);
         }
         System.out.println();
     }
