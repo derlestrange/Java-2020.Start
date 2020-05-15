69f5e8842e220b492c83c50f61286c0090ce8954
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-05-15 13:29:38 +0400

    correclty considered 1%

--- 06_InheritanceAndPolymorphism/06_AbstractClasses/src/BankClients/LegalPerson.java
+++ 06_InheritanceAndPolymorphism/06_AbstractClasses/src/BankClients/LegalPerson.java
@@ -21,9 +21,15 @@ public class LegalPerson extends Client {
     @Override
     public void takeMoney(double money) {
         double commission = money / 100;
-        System.out.println("Вы сняли: " + money);
-        System.out.println("Комиссия: " + commission);
-        setBankAccount(getBankAccount() - (commission + money));
-        System.out.println("Баланс: " + getBankAccount() + "\n");
+        if (commission > (getBankAccount() - money)) {
+            System.out.println("Недостаточно средств для списания с учетом комиссии 1%.");
+            System.out.println("Комиссия: " + commission + ", Сумма списания: " + money + "\n");
+            System.out.println("Баланс: " + getBankAccount());
+        } else {
+            System.out.println("Вы сняли: " + money);
+            System.out.println("Комиссия: " + commission);
+            setBankAccount(getBankAccount() - (money + commission));
+            System.out.println("Баланс: " + getBankAccount());
+        }
     }
 }
--- 06_InheritanceAndPolymorphism/06_BankAccount/src/Accounts/CardAccount.java
+++ 06_InheritanceAndPolymorphism/06_BankAccount/src/Accounts/CardAccount.java
@@ -8,15 +8,21 @@ public class CardAccount extends Account {
 
     @Override
     public void takeMoney(double money) {
-
         double commission = money / 100;
         if (money > getBankAccount()) {
             System.out.println("Недостаточно средств для списания!");
             System.out.println("Баланс: " + getBankAccount());
             return;
         }
-        System.out.println("Вы сняли: " + money);
-        System.out.println("Комиссия: " + commission);
-        setBankAccount(getBankAccount() - (commission + money));
+        if (commission > (getBankAccount() - money)) {
+            System.out.println("Недостаточно средств для списания с учетом комиссии 1%.");
+            System.out.println("Комиссия: " + commission + ", Сумма списания: " + money + "\n");
+            System.out.println("Баланс: " + getBankAccount());
+        } else {
+            System.out.println("Вы сняли: " + money);
+            System.out.println("Комиссия: " + commission);
+            setBankAccount(getBankAccount() - (money + commission));
+            System.out.println("Баланс: " + getBankAccount());
+        }
     }
 }
