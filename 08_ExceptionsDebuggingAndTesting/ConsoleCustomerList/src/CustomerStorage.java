1fb3dfbf84d6ae538cccb5d3fd143924a5d03f78
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-06-10 23:34:38 +0400

    exception handling

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -12,6 +12,10 @@ public class CustomerStorage
     public void addCustomer(String data)
     {
         String[] components = data.split("\\s+");
+        if (components.length != 4){
+            throw new ArrayIndexOutOfBoundsException("\nНеверный формат ввода. \nПравильный формат: " +
+                    "add Василий Петров \nvasily.petrov@gmail.com +79215637722\n");
+        }
         String name = components[0] + " " + components[1];
         storage.put(name, new Customer(name, components[3], components[2]));
     }
@@ -23,6 +27,9 @@ public class CustomerStorage
 
     public void removeCustomer(String name)
     {
+        if (!storage.containsKey(name)){
+            throw new ArrayIndexOutOfBoundsException("\nТакого клиента не существует! Попробуй снова\n");
+        }
         storage.remove(name);
     }
 
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -1,3 +1,5 @@
+import org.apache.commons.validator.routines.EmailValidator;
+
 import java.util.Scanner;
 
 public class Main
@@ -12,30 +14,29 @@ public class Main
 
     public static void main(String[] args)
     {
+        EmailValidator e = EmailValidator.getInstance();
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
         for(;;)
         {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("list")) {
-                executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
-            {
-                executor.removeCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
-                System.out.println(helpText);
-            }
-            else {
-                System.out.println(commandError);
+            try {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(commandError);
+                }
+            } catch (ArrayIndexOutOfBoundsException ex){
+                System.out.println(ex.getMessage());
             }
         }
     }
