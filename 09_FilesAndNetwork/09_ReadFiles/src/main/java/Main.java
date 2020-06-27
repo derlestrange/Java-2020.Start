4416c8b823a63c5867057618671a5328c3879fd2
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-06-27 16:49:10 +0400

    add size folder from console

--- 09_FilesAndNetwork/09_ReadFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/09_ReadFiles/src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter folder path: ");
+        for (; ; ) {
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();
+            Path path = Paths.get(s);
+            long size = Files.walk(path)
+                    .filter(p -> p.toFile().isFile())
+                    .mapToLong(p -> p.toFile().length())
+                    .sum();
+        }
     }
 }
