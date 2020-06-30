fd2ed0ab4e9ae103c0b145cab6c45f1393eaadd4
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-06-30 17:08:53 +0400

    made a with recursion

--- 09_FilesAndNetwork/09_ReadFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/09_ReadFiles/src/main/java/Main.java
@@ -4,10 +4,11 @@ import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
 import java.text.DecimalFormat;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -34,24 +35,35 @@ public class Main {
                     wrongFormatLogger.info(WRONG_FORMAT_LOGGER, "User input: " + userInput);
                     continue;
                 }
-                long size = Files.walk(Paths.get(userInput))
-                        .filter(p -> p.toFile().isFile())
-                        .mapToLong(p -> p.toFile().length())
-                        .sum();
+//                long size = Files.walk(Paths.get(userInput))
+//                        .filter(p -> p.toFile().isFile())
+//                        .mapToLong(p -> p.toFile().length())
+//                        .sum();
+
 
                 String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
-                int unitIndex = (int) (Math.log10(size) / 3);
+                int unitIndex = (int) (Math.log10(folderSize(new File(userInput))) / 3);
                 double unitValue = 1 << (unitIndex * 10);
 
                 String readableSize = new DecimalFormat("#,##0.#")
-                        .format(size / unitValue) + " "
+                        .format(folderSize(new File(userInput)) / unitValue) + " "
                         + units[unitIndex];
 
                 System.out.println("Folder weight: " + readableSize);
                 correctFormatLogger.info(CORRECT_FORMAT_LOGGER, "User input: " + userInput);
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public static long folderSize(File folder) {
+        long length = 0;
+        for (File file : Objects.requireNonNull(folder.listFiles())) {
+            if (file.isFile())
+                length += file.length();
+            else
+                length += folderSize(file);
+        }
+        return length;
     }
 }
