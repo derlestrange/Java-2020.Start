bb784a4511ce0b65bb0ac03838beea5d66a451f7
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-07-03 17:26:09 +0400

    one column ready

--- 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
+++ 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        
+
+    private static String csvFile = "data/movementList.csv";
+
+    public static void main(String[] args) throws IOException {
+        List<String> staff = new ArrayList<>();
+        List<String> lines = Files.readAllLines(Paths.get(csvFile));
+        for (String line : lines) {
+            String[] fragments = line.split(",", 6);
+            staff.add(fragments[3]);
+        }
+
+        staff.forEach(System.out::println);
     }
 }
