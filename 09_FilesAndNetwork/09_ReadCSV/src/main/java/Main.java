f1061b72371cfc8c1833b6f90ddbe27ad60b5261
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-07-05 17:54:51 +0400

    add spending and obtainment

--- 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
+++ 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
@@ -11,11 +11,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         List<String> staff = new ArrayList<>();
         List<String> lines = Files.readAllLines(Paths.get(csvFile));
+        double spending = 0;
+        double obtainment = 0;
         for (String line : lines) {
-            String[] fragments = line.split(",", 6);
-            staff.add(fragments[3]);
+            String[] fragments = line.split(",", 8);
+
+            String str = fragments[7].replaceAll(",", ".")
+                    .replaceAll("\"", "")
+                    .replaceAll("[А-я]", "0");
+            spending+=Double.parseDouble(str);
+
+            obtainment+=Double.parseDouble(fragments[6].replaceAll("[A-я]", "0"));
+            staff.add(str);
         }
 
-        staff.forEach(System.out::println);
+        System.out.println("Общий расход: " + spending);
+        System.out.println("Общий приход: " + obtainment);
+
+        //staff.forEach(System.out::println);
     }
 }
