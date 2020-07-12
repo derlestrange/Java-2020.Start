d46161e1923a83b51ff468d71846cae4c4929ff0
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-07-12 16:50:13 +0400

    correct code

--- 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
+++ 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class Main {
 
     private static String csvFile = "data/movementList.csv";
+    private static HashMap<String, Double> org = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        HashMap<String, Double> org = new HashMap<>();
+
         List<String> lines = Files.readAllLines(Paths.get(csvFile));
+
         double spending = 0;
         double obtainment = 0;
 
@@ -20,6 +22,7 @@ public class Main {
             String spendingDecimeter = fragments[7].replaceAll(",", ".")
                     .replaceAll("\"", "").trim();
             spending += Double.parseDouble(spendingDecimeter);
+            double spend = Double.parseDouble(spendingDecimeter);
 
             obtainment += Double.parseDouble(fragments[6]);
 
@@ -29,18 +32,18 @@ public class Main {
             String organization = transactSupply[transactSupply.length - 1].trim();
 
             if (!org.containsKey(organization)) {
-                org.put(organization, Double.parseDouble(spendingDecimeter));
+                org.put(organization, spend);
             } else {
                 double sum = org.get(organization);
-                sum += Double.parseDouble(spendingDecimeter);
+                sum += spend;
                 org.put(organization, sum);
             }
         }
 
 
         System.out.println("Общий расход: " + spending);
-        System.out.println("Общий приход: " + obtainment +"\n");
-        for (String s : org.keySet()){
+        System.out.println("Общий приход: " + obtainment + "\n");
+        for (String s : org.keySet()) {
             System.out.printf("%-30s %s %s %n", s, org.get(s), "руб.");
         }
 
