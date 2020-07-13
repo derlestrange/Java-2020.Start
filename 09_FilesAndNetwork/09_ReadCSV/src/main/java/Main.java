169bcfa38b536e0f4b963492262d0b88765fb096
derlestrange <56831326+derlestrange@users.noreply.github.com>
2020-07-13 23:42:37 +0400

    add obtain list

--- 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
+++ 09_FilesAndNetwork/09_ReadCSV/src/main/java/Main.java
@@ -8,7 +8,8 @@ import java.util.Map;
 public class Main {
 
     private static String csvFile = "data/movementList.csv";
-    private static HashMap<String, Double> orgHashMap = new HashMap<>();
+    private static HashMap<String, Double> orgHashMapSpend = new HashMap<>();
+    private static HashMap<String, Double> orgHashMapObtain = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get(csvFile));
@@ -25,8 +26,9 @@ public class Main {
             String spendingDecimeter = fragments[7].replace(",", ".")
                     .replaceAll("\"", "").trim();
             spending += Double.parseDouble(spendingDecimeter);
-            double spend = Double.parseDouble(spendingDecimeter);
+            double moneySpend = Double.parseDouble(spendingDecimeter);
 
+            double moneyObtainment = Double.parseDouble(fragments[6]);
             obtainment += Double.parseDouble(fragments[6]);
 
             String[] transaction = fragments[5].trim().split("\\s{3,}");
@@ -34,19 +36,31 @@ public class Main {
             String[] transactSupply = transact[transact.length - 1].split("\\\\");
             String organization = transactSupply[transactSupply.length - 1].trim();
 
-            if (!orgHashMap.containsKey(organization)) {
-                orgHashMap.put(organization, spend);
+            if (!orgHashMapSpend.containsKey(organization)) {
+                orgHashMapSpend.put(organization, moneySpend);
+                orgHashMapObtain.put(organization, moneyObtainment);
             } else {
-                double sum = orgHashMap.get(organization);
-                sum += spend;
-                orgHashMap.put(organization, sum);
+                double sumSpend = orgHashMapSpend.get(organization);
+                sumSpend += moneySpend;
+                orgHashMapSpend.put(organization, sumSpend);
+
+                double sumObtain = orgHashMapObtain.get(organization);
+                sumObtain += moneyObtainment;
+                orgHashMapObtain.put(organization, sumObtain);
             }
         }
 
         System.out.println("Общий расход: " + spending);
         System.out.println("Общий приход: " + obtainment + "\n");
+
         System.out.println("Суммы расходов по организациям:");
-        for (Map.Entry<String, Double> s : orgHashMap.entrySet()) {
+        for (Map.Entry<String, Double> s : orgHashMapSpend.entrySet()) {
+            System.out.printf("%-30s %s %s %n", s.getKey(), s.getValue(), "руб.");
+        }
+
+
+        System.out.println("\n=======================================\n\nСуммы прихода по организациям:");
+        for (Map.Entry<String, Double> s : orgHashMapObtain.entrySet()) {
             System.out.printf("%-30s %s %s %n", s.getKey(), s.getValue(), "руб.");
         }
     }
